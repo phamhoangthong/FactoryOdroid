1d984294f3ebad09ad3ba8123f1d93fcd3590858
HT pc <phamhoangthong@outlook.com>
2018-01-30 09:28:41 +0700

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,64 @@
+#include <jni.h>
+#include <string>
+#include "MSerialPort.h"
+
+class MySerialPort : public MSerialPort {
+private:
+    JNIEnv *m_env;
+    jobject m_parent;
+public:
+    void setInfo(JNIEnv *env,jobject parent) {
+        m_env = env;
+        m_parent = parent;
+    }
+
+    void haveData(char* buf, size_t size) {
+        jmethodID m_methodID = m_env->GetMethodID(m_env->GetObjectClass(m_parent), "sendDataToHandler", "([C)V");
+        jchar * p_buf = new jchar[size];
+        for(int i = 0; i < size; i++) {
+            p_buf[i] = (jchar)buf[i];
+        }
+        jcharArray m_buffer = m_env->NewCharArray(size);
+        m_env->SetCharArrayRegion(m_buffer,0,10,p_buf);
+        m_env->CallVoidMethod(m_parent, m_methodID, m_buffer);
+        delete[](p_buf);
+        m_env->DeleteLocalRef(m_buffer);
+    }
+};
+
+extern "C"
+JNIEXPORT jlong
+
+JNICALL
+Java_com_example_phamh_factoryodroid_MSerialCommunication_createSerial(
+        JNIEnv *env,
+        jobject parent, jstring port, jint speed) {
+    MySerialPort p_serial_port = MySerialPort();
+    const char *portString = env->GetStringUTFChars(port, JNI_FALSE);
+    p_serial_port.connect(portString, speed);
+    p_serial_port.setInfo(env, parent);
+    p_serial_port.start();
+    return (jlong)(&p_serial_port);
+}
+
+extern "C"
+JNIEXPORT jint
+
+JNICALL
+Java_com_example_phamh_factoryodroid_MSerialCommunication_sendToSerial(
+        JNIEnv *env,
+        jobject parent,jlong obj, jcharArray data) {
+    MySerialPort *p_serial_port = (MySerialPort *)(obj);
+    int m_size = env->GetArrayLength(data);
+    jchar *m_buf = new jchar[m_size];
+    env->GetCharArrayRegion(data,0,m_size,m_buf);
+    char* m_buffer = new char[m_size];
+    for (int i = 0; i < m_size; ++i) {
+        m_buffer[i] = m_buf[i];
+    }
+    p_serial_port->sendData(m_buffer, m_size);
+    delete[](m_buffer);
+    delete[](m_buf);
+    return 0;
+}
+
---
+++ app/src/main/java/com/example/phamh/factoryodroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.phamh.factoryodroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+
+}
